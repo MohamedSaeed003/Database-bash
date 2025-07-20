bd7e5ce850f2022b40bda0371dcd97fc6f971fff
MohamedSaeed003 <mohamed.saeed3033@gmail.com>
2025-07-20 19:20:07 +0300

    drop_table updated

--- drop_table.sh
+++ drop_table.sh
@@ -4,6 +4,7 @@ read -p "enter tabel name to remove " rm_table
 
 cd Databases/$db_name
 rm $rm_table
-rm .$rm_table
+rm .$rm_table.meta_data
 
-ls -a
+cd ..; cd ..
+echo "Table '$rm_table' has been removed successfully."
