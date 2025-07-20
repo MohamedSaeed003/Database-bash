6806f52f8f7d45f3197341d6ef697e8e68a8bf6a
youssef waheed <youssefwahid010@gmail.com>
2025-07-20 10:22:34 +0300

    update create_table file

--- create_table.sh
+++ create_table.sh
@@ -1,3 +1,37 @@
 #! /user/bin/bash
 
-echo "hello from create_table.sh"
+cd Databases/$db_name
+
+read -p "enter your table name: " table_name
+if [[ -f $table_name ]]
+then
+    echo "the table named '$table_name' already exist "
+    exit 1
+fi
+
+read -p "enter your column number: " col_numbers
+
+columns_line=""
+datatypes_line=""
+
+
+for (( i=1; i<=col_numbers; i++ ))
+do
+    read -p "enter column $i name: " col_name
+    read -p "enter column $i data type (int or string only): " col_type
+    columns+=("$col_name")
+    datatypes+=("$col_type")
+
+    if [[ $i == 1 ]]
+    then
+        columns_line="$col_name"
+        datatypes_line="$col_type"
+    else
+        columns_line="$columns_line:$col_name"
+        datatypes_line="$datatypes_line:$col_type"
+    fi
+done
+
+echo "$datatypes_line" > ".$table_name"
+echo "$columns_line" > "$table_name"
+echo "$datatypes_line" >> "$table_name"
