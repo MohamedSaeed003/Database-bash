24c05650037492cbffdb0f5b69bbde6113cea571
youssef waheed <youssefwahid010@gmail.com>
2025-07-20 10:20:35 +0300

    start update_table file

--- update_table.sh
+++ update_table.sh
@@ -1,3 +1,9 @@
 #! /user/bin/bash
 
-echo "hello from update_table.sh"
+
+read -p "enter table name need to update " table_name
+read -p "enter column name to search by " search_col
+read -p "enter value to search for " search_val
+read -p "enter column name to update " update_col
+read -p "enter new value " new_val
+
