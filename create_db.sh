9b440c81841ef025ec17041e6d7fedb1c2f02a9f
youssef waheed <youssefwahid010@gmail.com>
2025-07-21 12:26:06 +0300

    update & check validation for create_db file

--- create_db.sh
+++ create_db.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
+shopt -s extglob
 
-read -p "Enter the name of the new database: " db_name
+read -p "enter the name of the new database: " db_name
 
-mkdir -p Databases/"$db_name"
-echo "Creating database '$db_name'..."
+if [ -z "$db_name" ]                # -z check is database name is empty just pressed enter
+then
+    echo "invalid database name to be empty "
+
+elif [[ "$db_name" == *"/"* ]]
+then
+    echo "invalid database name cannot contain '/' use _ & - instead "
+elif [[ "$db_name" =~ ^_{3,} || "$db_name" =~ ^-{3,} ]]     #=~ match this regex, reject name that start with 3 or more _ or 3 or more -
+then
+    echo "invalid database name contain 3 or more of _ & -" 
+
+elif [ -e "Databases/$db_name" ]      # -e is for file or directory
+then
+    echo "database '$db_name' already exists "
+
+else
+
+case $db_name in
++(_) | +(-) )
+                echo "invalid database name cannot be only _ or -" 
+                ;;
+
++([A-Za-z0-9_-]) )
+                mkdir -p Databases/"$db_name"
+                echo "database $db_name has been created successfully"
+                ;;
+                
+*)    
+                echo "invalid database name: $db_name"
+                echo "avoid using spaces & special characters use letters, numbers, _ & - only "
+                ;;   
+esac
+
+fi
+
