1da320997c09661b12be6bc71b3fa410e5d6b0f9
youssef waheed <youssefwahid010@gmail.com>
2025-08-01 17:11:13 +0300

    add exit validation

--- create_db.sh
+++ create_db.sh
@@ -19,36 +19,48 @@ read -r -p "enter the name of the new database: " db_name   # -r read input with
 if [ -z "$db_name" ]                # -z check is database name is empty just pressed enter
 then
     echo "invalid database name to be empty "
+    echo "enter 0 to exit create database "
     continue
+
+elif [[ "$db_name" == '0' ]]
+then
+    echo "exit create database"
+    break
 
 elif [[ "$db_name" == *"/"* || "$db_name" == *'\'* || "$db_name" == *"-"* ]]  #  backslash skipping
 then
     echo "invalid database name cannot contain '/' or '\' or '-' use _ instead "
+        echo "enter 0 to exit create database "
     continue
 
 elif  echo "$db_name" | grep -qE '[_-]{2,}'   # reject name with 2 or more _ or 2 or more
 then
     echo "invalid database name contain 2 or more consecutive of _ & -" 
+        echo "enter 0 to exit create database "
     continue
 
 elif [ -e "Databases/$db_name" ]     # -e is for file or directory
 then
     echo "database '$db_name' already exists "
+        echo "enter 0 to exit create database "
     continue
 
 elif [[ "$db_name" =~ ^[0-9]+$ ]]
 then
     echo "invalid database name cannot be only numbers "        # this depend on user need 
+        echo "enter 0 to exit create database "
     continue
 
 elif ! [[ "$db_name" =~ [a-zA-Z] ]]
 then
     echo "invalid database name must contain at least letter "  # this depend on user need 
+        echo "enter 0 to exit create database "
     continue
 
 elif [[ $db_name =~ ^[0-9] ]]
 then
     echo "invalid database name cannot start with a number "
+        echo "enter 0 to exit create database "
     continue
 
 else
@@ -63,6 +75,7 @@ case $db_name in
                 
 *)    
                 echo "avoid using spaces & special characters use letters, numbers, _ only "
+                    echo "enter 0 to exit create database "
                 continue
                 ;;   
 esac
@@ -72,4 +85,4 @@ fi
 done
  
 
- # loop is last thing add this comment is last thing          
+ # loop is last thing add this comment is last thing              
